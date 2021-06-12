26de7812ec96164646e02e7a345504751a4c28a4
towry <tovvry@gmail.com>
2021-06-13 00:27:21 +0800

    Add matching-brackets

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,12 +1,31 @@
-const VALID: &[char] = &['[', ']', '(', ')', '{', '}'];
+const S: [char; 3] = ['(', '[', '{'];
+const E: [char; 3] = [')', ']', '}'];
+
+fn is_start(ch: &char) -> bool {
+    return "[({".find(*ch) != None
+}
+fn is_end(ch: &char) -> bool {
+    return "])}".find(*ch) != None
+}
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let is_valid = |&x: &char| VALID.contains(&x);
+    let mut stack: Vec<usize> = vec![];
 
-    !string.chars().any(|c| {
-        if !is_valid(&c) {
-            return false;
+    for c in string.chars() {
+        if is_start(&c) {
+            stack.push(S.iter().position(|&s| s == c).unwrap());
+        } else if is_end(&c) {
+            // check if last one is match
+            let last = stack.pop();
+            if last != E.iter().position(|&s| s == c) {
+               return false;
+            }
         }
-        string.matches(c).count() % 2 != 0
-    })
+    }
+
+    if stack.len() == 0 {
+        return true;
+    }
+
+    false
 }
