b7645cde8a4bbf53663d43e13c42b4039ea9d55a
towry <tovvry@gmail.com>
2021-06-13 00:38:20 +0800

    add clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,35 +1,15 @@
-use std::fmt::{Display, Formatter, Result as FmtResult};
-use std::cmp::Eq;
-
-#[derive(Debug)]
-pub struct Clock {
-    hours: i32,
-    minutes: i32,
-}
-
-impl PartialEq for Clock {
-    fn eq(&self, other: &Self) -> bool {
-        self.hours == other.hours && self.minutes == other.minutes
-    }
-}
-
-impl Eq for Clock {}
-
-impl Display for Clock {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        write!(f, "{}:{}", self.hours, self.minutes)
-    }
-}
+pub struct Clock;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock { hours, minutes }
+        unimplemented!(
+            "Construct a new Clock from {} hours and {} minutes",
+            hours,
+            minutes
+        );
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock {
-            hours: self.hours,
-            minutes: self.minutes + minutes
-        }
+        unimplemented!("Add {} minutes to existing Clock time", minutes);
     }
 }
