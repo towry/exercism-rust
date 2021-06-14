dc51a46b36f2858ef7ada1a673d6b6b88726ba3e
towry <tovvry@gmail.com>
2021-06-14 22:16:23 +0800

    Update clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,38 @@
-pub struct Clock;
+use std::fmt;
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let hours = self.hours;
+        let minutes = self.minutes;
+
+        return write!(f, "{}:{}", format!("{:0width$}", hours, width=2), format!("{:0width$}", minutes, width=2));
+    }
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let add_to_hours = (minutes as f32 / 60.0).floor() as i32;
+        let correct_minutes = minutes % 60;
+        let mut our_hours = hours + add_to_hours;
+        our_hours = our_hours % 24;
+
+        return Clock {
+            hours: our_hours,
+            minutes: correct_minutes
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        let self_minutes = self.minutes + minutes;
+
+        return Clock::new(self.hours, self_minutes);
     }
 }
