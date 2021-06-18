6adc67eb0d89c7af017965f4aa6bad38c118e280
towry <tovvry@gmail.com>
2021-06-18 17:41:54 +0800

    Finish clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,38 +1,32 @@
 use std::fmt;
 
-#[derive(PartialEq)]
-#[derive(Debug)]
+const DAY: i64 = 24 * 60;
+const HOUR: i64 = 60;
+
+#[derive(Debug, Eq, PartialEq)]
 pub struct Clock {
-    hours: i32,
-    minutes: i32,
+    minutes: i64,
 }
 
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let hours = self.hours;
-        let minutes = self.minutes;
+        let hours = self.minutes / HOUR;
+        let minutes = self.minutes % HOUR;
 
         return write!(f, "{}:{}", format!("{:0width$}", hours, width=2), format!("{:0width$}", minutes, width=2));
     }
 }
 
-impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        let add_to_hours = (minutes as f32 / 60.0).floor() as i32;
-        let correct_minutes = minutes % 60;
-        let mut our_hours = hours + add_to_hours;
-        our_hours = our_hours % 24;
 
-        return Clock {
-            hours: our_hours,
-            minutes: correct_minutes
+impl Clock {
+    pub fn new(hours: i64, minutes: i64) -> Self {
+        Clock {
+            minutes: (((hours * HOUR + minutes) % DAY) + DAY) % DAY
         }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        let self_minutes = self.minutes + minutes;
-
-        return Clock::new(self.hours, self_minutes);
+    pub fn add_minutes(&self, minutes: i64) -> Self {
+        return Clock::new(0, self.minutes + minutes);
     }
 }
